c6ce2dd2a5e7cc21e620a044f8673e0b72103386
Eugene Agesa <eugeneagesa734@gmail.com>
2025-10-27 10:21:46 +0300

    temperature converion

---
+++ string-methods/index.js
@@ -0,0 +1,4 @@
+const fullName = "MaryJane Watson";
+
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+let lastName = fullName.slice(fullName.indexOf(" ") + 1);
---
+++ switch-statement/index.js
@@ -0,0 +1,21 @@
+let testscore = 33;
+let letterGrade;
+
+switch (true) {
+    case testscore >= 90: 
+        letterGrade = "A";
+        break;
+    case testscore >= 80:
+        letterGrade = "B";
+        break;
+    case testscore >= 70:
+        letterGrade = "C";
+        break;
+    case testscore >= 60:
+        letterGrade = "D";
+        break;
+    default:
+        letterGrade = "F";
+}
+
+console.log (letterGrade);
